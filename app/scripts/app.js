cf5279f0d8f0c242a5b665212a085ffcd51e0f69
system_user <john@schweikert.net>
2015-10-14 09:37:15 -0400

    converted to search panel

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 (function(document) {
   'use strict';
 
-  // Grab a reference to our auto-binding template
+/*  // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
@@ -70,5 +70,5 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   app.scrollPageToTop = function() {
     document.getElementById('mainContainer').scrollTop = 0;
   };
-
+*/
 })(document);
